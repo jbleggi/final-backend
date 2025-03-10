f4b17ee3bad71cef237dfce24b0fba58c0e0ff08
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-10 14:21:36 -0400

    Expanded first test

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -3,8 +3,18 @@ require 'rails_helper'
 
 describe "Subscriptions API" do 
   it "sends a list of subscriptions" do 
+    Subscription.create(status: "active", cost: 20.00)
+    Subscription.create(status: "active", cost: 25.00)
+    Subscription.create(status: "canceled", cost: 20.00)
+    Subscription.create(status: "paused", cost: 10.00)
+    Subscription.create(status: "paused", cost: 200.00)
+
     get '/api/v1/subscriptions'
 
     expect(response).to be_successful
+
+    subscriptions = JSON.parse(response.body, symbolize_names: true)
+
+    expect(subscriptions.count).to eq(5)
   end
 end
