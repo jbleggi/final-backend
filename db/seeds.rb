28b0f772ba8cebdd8d727713ab0afeb666fe49f9
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-10 15:04:48 -0400

    Added 5 seeded subscriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Subscription.create(status: "active", cost: 20.00)
+Subscription.create(status: "active", cost: 25.00)
+Subscription.create(status: "canceled", cost: 20.00)
+Subscription.create(status: "paused", cost: 10.00)
+Subscription.create(status: "paused", cost: 200.00)
