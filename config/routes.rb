8c96cf9bdb7566d2862e47fd54d25499e15f6e00
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-10 15:20:35 -0400

    Added namespacing to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   
-  resources :subscriptions, only: [:index, :show, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :subscriptions, only: [:index, :show, :destroy]
+    end
+  end  
 end
