7f719798dea1cdabacbd5936bd481ed19709cc1d
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-10 22:24:43 -0400

    Add #show

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -4,4 +4,7 @@ class Api::V1::SubscriptionsController < ApplicationController
     render json: SubscriptionSerializer.new(subscriptions)
   end
   
+  def show
+    render json: SubscriptionSerializer.new(Subscription.find(params[:id]))
+  end
 end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 
 describe "Subscriptions API" do 
-  it "sends a list of subscriptions" do
+  it "can send a list of subscriptions" do
     get api_v1_subscriptions_path
 
     expect(response).to be_successful
@@ -17,4 +17,20 @@ describe "Subscriptions API" do
       expect(subscription[:attributes]).to have_key(:cost)
     end
   end
+
+  it "can retrieve one subscription based on id" do
+    id = Subscription.first.id
+
+    get "/api/v1/subscriptions/#{id}"
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(result[:data][:attributes]).to have_key(:id)
+    expect(result[:data][:attributes]).to have_key(:status)
+    expect(result[:data][:attributes]).to have_key(:cost)
+    expect(result[:data][:attributes][:id]).to eq id
+  end
+
 end
