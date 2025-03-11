355ae27819f8fc01784465ab4c571e7b8f50f837
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-10 22:17:18 -0400

    Had to run RAILS_ENV=test bin/rails db:setup to set up first passing RSpec.

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -6,20 +6,15 @@ describe "Subscriptions API" do
     get api_v1_subscriptions_path
 
     expect(response).to be_successful
-
+    
     results = JSON.parse(response.body, symbolize_names: true)
 
-    expect(results.count).to eq(5)
+    expect(results[:data].count).to eq(5)
 
-    results.each do |subscription|
-      expect(subscription).to have_key(:id)
-      expect(subscription.id).to be_an Integer
-
-      expect(subscription).to have_key(:status)
-      expect(subscription.status).to be_a String
-
-      expect(subscription).to have_key(:cost)
-      expect(subscription.cost).to be_a Decimal
+    results[:data].each do |subscription|
+      expect(subscription[:attributes]).to have_key(:id)
+      expect(subscription[:attributes]).to have_key(:status)
+      expect(subscription[:attributes]).to have_key(:cost)
     end
   end
 end
