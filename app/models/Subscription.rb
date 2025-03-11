20dfa764f706c8254995da0191f293fb98965543
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-11 11:39:29 -0400

    Update model validation

--- app/models/Subscription.rb
+++ app/models/Subscription.rb
@@ -1,10 +1,11 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
-  has_and_belongs_to_many :items, through: :items_subscriptions
+  has_and_belongs_to_many :items, join_table: :items_subscriptions
   has_many :items_subscriptions
 
-  validates :status, inclusion: { in: ['active', 'canceled'], message: "%{value} is not a valid status" }, presence: true
+  validates_inclusion_of :status, in: ['active', 'canceled'], message: "%{value} is not a valid status"
 end
+
 
 # subscription.items to access items in a subscription
 # subscription.customer to access cust associated with a subs
