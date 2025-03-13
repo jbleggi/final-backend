3a27599946b5a6370c654c14c524aa5591753e09
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-12 22:35:27 -0400

    Customized Customer Serializer for FE Display

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,12 @@
 class CustomerSerializer
   include JSONAPI::Serializer
   attributes :id, :first_name, :last_name, :email, :address_city, :address_state, :address_zip
+
+  attribute :full_name do |customer|
+    "#{customer.first_name} #{customer.last_name}"
+  end
+
+  attribute :address_summary do |customer|
+    "#{customer.address_city}, #{customer.address_state} #{customer.address_zip}"
+  end
 end
